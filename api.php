10b84deed373466fcd3759a17ec745e26bc994c0
Wojtek Gajewski <wgajew@yola.pl>
2017-02-06 00:06:47 +0100

    api methods description

--- api.php
+++ api.php
@@ -235,97 +235,267 @@ bubble_settings: Object, optional
 <br>
 <h3>ComicBubbles object methods</h3>
 <h4>getBubbleById(bubble_id)<span>returns</span><span>Bubble</span></h4>
+<p>
+bubble_id: String, required
+</p>
 <h4>getBubblesByName(bubble_name)<span>returns</span><span>Bubble[]</span></h4>
+<p>
+bubble_name: String, required
+</p>
 <h4>getAllBubbles()<span>returns</span><span>Bubble[]</span></h4>
 <h4>addBubble(bubble,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+bubble: Bubble, required<br>
+callback: function, optional
+</p>
 <h4>setText(text)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+text: String, required
+</p>
 <h4>getText()<span>returns</span><span>String</span></h4>
 <h4>setWidth(width)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+width: Number, required
+</p>
 <h4>getWidth()<span>returns</span><span>Number</span></h4>
 <h4>setHeight(height)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+height: Number, required
+</p>
 <h4>getHeight()<span>returns</span><span>Number</span></h4>
 <h4>setFontFamily(font_family)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+font_family: String, required
+</p>
 <h4>getFontFamily()<span>returns</span><span>String</span></h4>
 <h4>setFontSize(font_size)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+font_size: Number, required
+</p>
 <h4>getFontSize()<span>returns</span><span>String</span></h4>
 <h4>setFontStyle(font_style)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+font_style: String, required
+</p>
 <h4>getFontStyle()<span>returns</span><span>String</span></h4>
 <h4>setFontWeight(font_weight)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+font_weight: String, required
+</p>
 <h4>getFontWeight()<span>returns</span><span>String</span></h4>
 <h4>setTextAlign(text_align)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+text_align: String, required
+</p>
 <h4>getTextAlign()<span>returns</span><span>String</span></h4>
 <h4>setBackground(background_color)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+background_color: String, required
+</p>
 <h4>getBackground()<span>returns</span><span>String</span></h4>
 <h4>setColor(text_color)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+text_color: String, required
+</p>
 <h4>getColor()<span>returns</span><span>String</span></h4>
 <h4>setOpacity(opacity)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+opacity: Number, required
+</p>
 <h4>getOpacity()<span>returns</span><span>Number</span></h4>
 <h4>setReadonly(readonly)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+readonly: Boolean, required
+</p>
 <h4>isReadonly()<span>returns</span><span>Boolean</span></h4>
 <h4>setSettable(settable)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+settable: Boolean, required
+</p>
 <h4>isSettable()<span>returns</span><span>Boolean</span></h4>
 <h4>setBubbleStyle(bubble_style)<span>returns</span><span>ComicBubbles</span></h4>
+<p>
+bubble_style: String, required
+</p>
 <h4>getBubbleStyle()<span>returns</span><span>String</span></h4>
 <h4>getDefaultSettings()<span>returns</span><span>Object</span></h4>
 <h4>onBubbleStateChange(callback)</h4>
+<p>
+callback: function, required
+</p>
 <h4>getBubblesData()<span>returns</span><span>Object</span></h4>
 <h3>Bubble object methods</h3>
 <h4>setId(id,callback)<span>returns</span><span>Bubble</span></h4>
 <p>
-id: string, required<br>
+id: String, required<br>
 callback: function, optional
 </p>
 <h4>getId()<span>returns</span><span>String</span></h4>
 <h4>setName(name,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+name: String, required<br>
+callback: function, optional
+</p>
 <h4>getName()<span>returns</span><span>String</span></h4>
 <h4>setText(text,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+text: String, required<br>
+callback: function, optional
+</p>
 <h4>getText()<span>returns</span><span>String</span></h4>
 <h4>setX(x,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+x: Number, required<br>
+callback: function, optional
+</p>
 <h4>getX()<span>returns</span><span>Number</span></h4>
 <h4>setY(y,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+y: Number, required<br>
+callback: function, optional
+</p>
 <h4>getY()<span>returns</span><span>Number</span></h4>
 <h4>setWidth(width,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+width: Number, required<br>
+callback: function, optional
+</p>
 <h4>getWidth()<span>returns</span><span>Number</span></h4>
 <h4>setHeight(height,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+height: Number, required<br>
+callback: function, optional
+</p>
 <h4>getHeight()<span>returns</span><span>Number</span></h4>
 <h4>setFontFamily(font_family,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+font_family: String, required<br>
+callback: function, optional
+</p>
 <h4>getFontFamily()<span>returns</span><span>String</span></h4>
 <h4>setFontSize(font_size,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+font_size: Number, required<br>
+callback: function, optional
+</p>
 <h4>getFontSize()<span>returns</span><span>Number</span></h4>
 <h4>setFontStyle(font_style,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+font_style: String, required<br>
+callback: function, optional
+</p>
 <h4>getFontStyle()<span>returns</span><span>String</span></h4>
 <h4>setFontWeight(font_weight,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+font_weight: String, required<br>
+callback: function, optional
+</p>
 <h4>getFontWeight()<span>returns</span><span>String</span></h4>
 <h4>setTextAlign(text_align,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+text_align: String, required<br>
+callback: function, optional
+</p>
 <h4>getTextAlign()<span>returns</span><span>String</span></h4>
 <h4>setBackground(background_color,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+background_color: String, required<br>
+callback: function, optional
+</p>
 <h4>getBackground()<span>returns</span><span>String</span></h4>
 <h4>setColor(text_color,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+text_color: String, required<br>
+callback: function, optional
+</p>
 <h4>getColor()<span>returns</span><span>String</span></h4>
 <h4>setOpacity(opacity,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+opacity: Number, required<br>
+callback: function, optional
+</p>
 <h4>getOpacity()<span>returns</span><span>Number</span></h4>
 <h4>setReadonly(readonly,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+readonly: Boolean, required<br>
+callback: function, optional
+</p>
 <h4>isReadonly()<span>returns</span><span>Boolean</span></h4>
 <h4>setSettable(settable,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+settable: Boolean, required<br>
+callback: function, optional
+</p>
 <h4>isSettable()<span>returns</span><span>Boolean</span></h4>
 <h4>setTailLocation(tail_location,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+tail_location: String, required<br>
+callback: function, optional
+</p>
 <h4>getTailLocation()<span>returns</span><span>String</span></h4>
 <h4>setTailX(tail_x,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+tail_x: Number, required<br>
+callback: function, optional
+</p>
 <h4>getTailX()<span>returns</span><span>Number</span></h4>
 <h4>setTailY(tail_y,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+tail_y: Number, required<br>
+callback: function, optional
+</p>
 <h4>getTailY()<span>returns</span><span>Number</span></h4>
 <h4>setBubbleStyle(bubble_style,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+bubble_style: String, required<br>
+callback: function, optional
+</p>
 <h4>getBubbleStyle()<span>returns</span><span>String</span></h4>
 <h4>show(callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+callback: function, optional
+</p>
 <h4>hide(callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+callback: function, optional
+</p>
 <h4>isVisible()<span>returns</span><span>Boolean</span></h4>
 <h4>fadeIn(miliseconds,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+miliseconds: Number, optional<br>
+callback: function, optional
+</p>
 <h4>fadeOut(miliseconds,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+miliseconds: Number, optional<br>
+callback: function, optional
+</p>
 <h4>moveTo(x, y, miliseconds, callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+x: Number, required<br>
+y: Number, required<br>
+miliseconds: Number, optional<br>
+callback: function, optional
+</p>
 <h4>delay(miliseconds,callback)<span>returns</span><span>Bubble</span></h4>
+<p>
+miliseconds: Number, optional<br>
+callback: function, optional
+</p>
 <h4>remove(callback)</h4>
+<p>
+callback: function, optional
+</p>
 <h4>onMouseEvent(callback, event)<span>returns</span><span>Bubble</span></h4>
+<p>
+callback: function, required<br>
+event: String, optional
+</p>
 <h4>offMouseEvent(callback, event)<span>returns</span><span>Bubble</span></h4>
+<p>
+callback: function, required<br>
+event: String, optional
+</p>
 <h4>getBubbleSettings()<span>returns</span><span>Function</span></h4>
 </div>
 </body>
